f3c1a554c0cb38f810ccb20a9c18189578c7ad8e
Nevedimca <Nevedimca@gmail.com>
2022-06-16 11:09:41 +0300

    Test

---
+++ JS_group_30_test.js
@@ -0,0 +1,43 @@
+// let names = "Oleksandr" 
+
+// console.log(names)
+
+// let age = 33
+// console.log(age)
+
+// let person = names + " " +age
+// console.log(person)
+
+
+// console.log(names + " " +age)
+
+// let user_age = 33
+// let work_period = 10
+// let total_time = user_age + work_period
+// console.log("total_time =", total_time, typeof(total_time))
+
+// let total_time2 = user_age * work_period
+// console.log("total_time2 =", total_time2, typeof(total_time2))
+
+// let total_time3 = user_age / work_period
+// console.log("total_time3 =", total_time3, typeof(total_time3))
+
+// let morning = false
+// console.log("morning =", morning, typeof(morning))
+
+// let evening = true
+// console.log("evening =", evening, typeof(evening))
+
+let apples = 15
+let cherry = 20
+let compare = apples > cherry
+console.log ("compare =", compare)
+
+if (compare){
+    console.log("compare is", compare)
+}    else if (apples == 15) {
+    console.log("apples == ", apples)
+} else {
+    console.log("else compare is", compare)
+}
+
