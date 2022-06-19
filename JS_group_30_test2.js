e006996bb0466e944098b7a01505bf9effeda49b
Nevedimca <Nevedimca@gmail.com>
2022-06-19 20:55:10 +0300

    add

---
+++ JS_group_30_test2.js
@@ -0,0 +1,90 @@
+// console.log ('hello')
+
+// console.log ("||")
+// console.log (true || false)
+// console.log (false || true)
+// console.log (true || false || false)
+// console.log (false || false || false)
+// console.log ("&&")
+// console.log (true && false)
+// console.log (true && false && false)
+console.log ("!, &, ^, |, &&, ||")
+// console.log (true && false || true)
+// console.log (true && false || true && false)
+
+// if (false || true) {
+//     console.log ("1")
+// }else {
+//     console.log ("0")
+// }
+
+// if (false && true) {
+//     console.log ("1")
+// }else {
+//     console.log ("0")
+// }
+
+// if (false || true && false) {
+//     console.log ("1")
+// }else {
+//     console.log ("0")
+// }
+
+
+// if (false && true && false) {
+//     console.log ("1")
+// }else {
+//     console.log ("0")
+// }
+
+// let age = 18
+// let money = 1000
+// let dog = false
+
+// if (age >= 18 && money >= 1000 && dog) {
+//     console.log ('get tiket')
+// } else if (age < 18) {
+//     console.log ('early')
+// } if (money < 1000) {
+//     console.log ('keep calm and work hard!!')
+// } if (!dog) {
+//     console.log ('get cat')
+// }
+
+
+// let dog_show = true
+
+// switch (dog_show) {
+//     case age >= 18 : 
+//     console.log ('time to win')
+//     break;
+//     case money >= 1000 : 
+//     console.log ('rich')
+//     break;
+// }
+
+// count = 0
+// while  (count < 10) {
+//     // console.log (count)
+//     if (count % 2 == 0) {
+//         console.log (count, "even")
+//     }
+//     count ++
+// }
+
+// for (let i = 0; i < 10; i++){
+//     console.log (i)
+// }
+
+// mm = [1,2,3,4,5,6,7,6,5,34,3,6,8]
+// for (let i = 0; i < 15; i++){
+//     console.log (mm[i], "win")
+// }
+
+mm = ["alla","sanya", "milla", "anatoliy", "dmitriy"]
+console.log ("mm =", mm.length)
+for (let i = 0; i < mm.length; i++){
+    let b = i
+    b++
+    console.log (b,i, mm[i], "win")
+}
