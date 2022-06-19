e63c4ef7bfde9c0366d374e7f3652f25cd282a36
Nevedimca <Nevedimca@gmail.com>
2022-06-19 15:45:09 +0300

    add

--- JSHW_1.js
+++ JSHW_1.js
@@ -76,7 +76,7 @@ item_7_type = typeof(item_7)
 console.log ("25. item_7 =", item_7,  ">переменная>",typeof(item_7),"/", "item_7_type =", item_7_type, ">переменная>",typeof(item_7_type),)
 
 //  26. Создать переменную “age_1” и присвоить ей значение 10
-let age_1 = 60
+let age_1 = 10
 
 //  27. Создать переменную “age_2” и присвоить ей значение 18
 let age_2 = 18
@@ -137,7 +137,7 @@ console.log("33. Technical work")
 
     //  33v2. Иначе выводите “Technical work”.
     
-    if (age_1 == age_3 || age_1 == 0) {
+    if (age_1 == age_3 || age_1 <= 0 ) {
     console.log("33v2. Technical work")
     } else if (age_1 < age_2){
         console.log("33v2. You don’t have access cause your age is " + age_1 + " It’s less then " +age_2)
