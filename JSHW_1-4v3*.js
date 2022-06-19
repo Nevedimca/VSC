9c0c4c68b13ba8bc3cd3447f7a86f45db78cd184
Nevedimca <Nevedimca@gmail.com>
2022-06-20 02:12:06 +0300

    add

--- JSHW_1-4v3*.js
+++ JSHW_1-4v3*.js
@@ -4,26 +4,27 @@
 
 let age_02 = 18
 let age_03 = 60
+const div = document.getElementById('d')
 const checkAge = function(...age_01) {
     console.log(age_01)
     age_01.forEach(el => {
         if (el && !isNaN(el)) {
             if (el < age_02){
-                console.log(el,'\n',"4v2*. You don’t have access cause your age is " + el + " It’s less then " +age_02)
-                // alert("4*. You don’t have access cause your age is " + age_01 + " It’s less then " +age_02)
+                console.log(el+ '\n'+ "4v3*. You don’t have access cause your age is " + el + " It’s less then " +age_02)
+                div.innerHTML += el + '<br />'+ "4v3*. You don’t have access cause your age is " + el + " It’s less then " +age_02 +'<br />'
             } else if (el >= age_02 && el < age_03) {
-                console.log(el,'\n',"4v2*. Welcome  !")
-                // alert("4*. Welcome  !")
+                console.log(el+ '\n'+ "4v3*. Welcome  !")
+                div.innerHTML += el + '<br />'+ "4v3*. Welcome  !"+'<br />'
             } else if (el > age_03) {
-                console.log(el,'\n',"4v2*. Keep calm and look Culture channel")
-                // alert("4*. Keep calm and look Culture channel")
+                console.log(el+ '\n'+ "4v3*. Keep calm and look Culture channel")
+                div.innerHTML += el + '<br />'+ "4v3*. Keep calm and look Culture channel"+'<br />'
             } else {
-                console.log (el,'\n',"4v2*. Technical work")
-                // alert ("4*. Technical work")
+                console.log (el+ '\n'+ "4v3*. Technical work")
+                div.innerHTML += el + '<br />'+ "4v3*. Technical work"+'<br />'
             }
             }else{
-                console.log (el,'\n',"4v2*. error number")
-                // alert("4*. error number")
+                console.log (el+ '\n'+ "4v3*. error number")
+                div.innerHTML+= el + '<br />'+ "4v3*. error number"+'<br />'
             } 
     });
 }
