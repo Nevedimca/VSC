1fb4a908182b16551b294f3e0fc8e59998745684
Nevedimca <Nevedimca@gmail.com>
2022-06-23 23:35:31 +0300

    add

--- JSHW_2-1.js
+++ JSHW_2-1.js
@@ -3,17 +3,15 @@
 // 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1
 
 
-let p = 2
-const exponent = (exp) =>{
-    console.log ("Возведения", p, "в степень", exp)
-for (let i = 1; i <= exp; i++){
-    console.log (`${i} - ${p ** i}`)
+let voz = 2
+let exp = 10
+if (exp <1) {
+    console.log ("Степень", exp, "вводить нельзя!")
+} else {
+    console.log ("Возведения", voz, "в степень", exp, "=", Math.pow(p, exp)) // Math.pow(base, exponent) - Возведение в степень, base - Основание степени, exponent - Показатель степени
+for (let i = 1; i <= exp; i++){  //for (начало; условие; шаг) {тело цикла}
+    console.log (`${i} - ${voz ** i}`) // Возведение в степень **
+    }
 }
-}
-
-exponent(10)
-exponent(-2)
-exponent(0)
-exponent(4)
 
 
---
+++ JS_group_30_test3.js
@@ -0,0 +1,9 @@
+let t1 = 10
+let t2 = 20
+let t3 = 35
+
+let summ = (n1,n2) => {
+    console.log(`sum = ${n1+n2}`)
+}
+
+summ (t1,t3)
