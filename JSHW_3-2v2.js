73d27f7a2a041923a74afc724a930894a6af6596
Nevedimca <Nevedimca@gmail.com>
2022-06-29 22:26:36 +0300

    add

--- JSHW_3-2v2.js
+++ JSHW_3-2v2.js
@@ -8,18 +8,14 @@
 // Task 2*** Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs. для дальнейшего использования в функции, описанной в задании
 
 
-console.log ('----- Задача JSHW_3-2 -----')
+console.log ('----- Задача JSHW_3-2v2 -----')
 const fs = require('fs')
 let user = JSON.parse(fs.readFileSync('task2.json'))
 
 // console.log(user)
 
-function uniqueFunc1(){
-    let unique1 = []
-    user.forEach(el => {
-        if (!unique1.includes(JSON.stringify(el))) unique1.push(JSON.stringify(el))
-    })
-    return unique1.map(el => JSON.parse(el))
-}
-console.log(uniqueFunc1())
+let unique = Array.from (new Set(user.map((item)=>JSON.stringify(item)))).map((item) => JSON.parse(item))
+console.log(unique.length)
+
+
 
--- tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs')
-let user = JSON.parse(fs.readFileSync('task2.json'))
